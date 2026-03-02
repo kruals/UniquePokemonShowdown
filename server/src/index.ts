0708602e0b3a7ba92b0264c3754991887d502708
kruals <145916088+kruals@users.noreply.github.com>
2026-03-02 10:40:47 +0400

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "http://localhost:3000" } });
+const io = new Server(server, { cors: { origin: process.env.CLIENT_URL } });
 
 app.use(cors());
 app.use(express.json());
