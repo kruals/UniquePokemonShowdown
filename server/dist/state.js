8d94ba91b5b97ba4b3c7a476d9a0aa0338ac251c
kruals <145916088+kruals@users.noreply.github.com>
2026-03-02 16:29:22 +0300

    Delete server/dist directory

--- server/dist/state.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getOnlineUsernames = exports.onlineUsers = void 0;
-// Храним ID сокетов по именам пользователей
-exports.onlineUsers = new Map();
-// Функция для получения списка имен (для фронтенда)
-const getOnlineUsernames = () => Array.from(exports.onlineUsers.keys());
-exports.getOnlineUsernames = getOnlineUsernames;
